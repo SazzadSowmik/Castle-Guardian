f56a73a02fefdc395ad827acd0fe8aad55c2816e
MdSazzadul-Isla <>
2023-01-21 14:28:01 +0000

    fixing conflict

--- music_cog.py
+++ music_cog.py
@@ -10,7 +10,6 @@ class music_cog(commands.Cog):
 
     self.is_playing = False
 
-
     self.music_queue = []
     self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
     self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 - reconnect_delay_max 5', 'options': '-vn' }
